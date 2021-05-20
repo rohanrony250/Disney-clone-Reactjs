f4d59d8546c86b83fc555c498e8e76256da6f480
rohanrony250 <rohanrony@hotmail.com>
2021-05-20 21:53:34 +0300

    user-details-redux

--- src/components/Header-Component.jsx
+++ src/components/Header-Component.jsx
@@ -1,21 +1,57 @@
 import React from "react"
 import styled from "styled-components"
 import { auth, provider } from "../Firebase/firebase"
+import {useDispatch, useSelector} from "react-redux"
+import {useHistory} from "react-router-dom"
+import {selectUserName, selectUserPhoto,setUserLoginDetails, setSignOutState} from "../features/user/userSlice"
 
 const Header = (props) =>
 {
-    
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const username = useSelector(selectUserName)
+    const userPhoto = useSelector(selectUserPhoto)
     const handleAuth = () =>
     {
         auth.signInWithPopup(provider).then((result) =>
         {
-            console.log(result);
+            setUser(result.user);
         }).catch((err) =>
         {
             console.log(err.message)
         })
     }
     
+    const handleLogout = () =>
+    {
+        auth.signOut().then((result)=>
+        {
+            console.log("user signed out")
+            setUser("logout")
+
+        }).catch((err)=>
+        {
+            console.log(err.message)
+        })
+    }
+
+    const setUser = (user) =>
+    {
+        
+        if(user == "logout")
+        {
+            dispatch(setSignOutState())
+        }
+        
+        dispatch(
+            setUserLoginDetails(
+            {
+                name: user.displayName,
+                photo: user.photoURL, 
+                email : user.email
+            }
+        ))
+    }
     
     
     return (
@@ -23,47 +59,55 @@ const Header = (props) =>
             <Logo>
                 <img src ="images/logo.svg" alt="Disney+"/>
             </Logo>
-            <NavMenu>
-                <a href = "/home">
-                    <img src="images/home-icon.svg" alt="HOME"/>
-                    <span>
-                        HOME
-                    </span>
-                </a>
-                <a href = "/home">
-                    <img src="images/search-icon.svg" alt="HOME"/>
-                    <span>
-                        SEARCH
-                    </span>
-                </a>
-                <a href = "/home">
-                    <img src="images/watchlist-icon.svg" alt="HOME"/>
-                    <span>
-                        WATCHLIST
-                    </span>
-                </a>
-                <a href = "/home">
-                    <img src="images/original-icon.svg" alt="HOME"/>
-                    <span>
-                        ORIGINALS
-                    </span>
-                </a>
-                <a href = "/home">
-                    <img src="images/movie-icon.svg" alt="HOME"/>
-                    <span>
-                        MOVIES
-                    </span>
-                </a>
-                <a href = "/home">
-                    <img src="images/series-icon.svg" alt="HOME"/>
-                    <span>
-                        SERIES
-                    </span>
-                </a>                
-            </NavMenu>
-            <Login onClick = {handleAuth}>
-                LOGIN
-            </Login>
+
+            {
+
+                !username ? 
+                    <Login onClick={handleAuth}>LOGIN</Login>
+                :
+                <>
+                    <NavMenu>
+                        <a href = "/home">
+                            <img src="images/home-icon.svg" alt="HOME"/>
+                            <span>
+                                HOME
+                            </span>
+                        </a>
+                        <a href = "/home">
+                            <img src="images/search-icon.svg" alt="HOME"/>
+                            <span>
+                                SEARCH
+                            </span>
+                        </a>
+                        <a href = "/home">
+                            <img src="images/watchlist-icon.svg" alt="HOME"/>
+                            <span>
+                                WATCHLIST
+                            </span>
+                        </a>
+                        <a href = "/home">
+                            <img src="images/original-icon.svg" alt="HOME"/>
+                            <span>
+                                ORIGINALS
+                            </span>
+                        </a>
+                        <a href = "/home">
+                            <img src="images/movie-icon.svg" alt="HOME"/>
+                            <span>
+                                MOVIES
+                            </span>
+                        </a>
+                        <a href = "/home">
+                            <img src="images/series-icon.svg" alt="HOME"/>
+                            <span>
+                                SERIES
+                            </span>
+                        </a>                
+                    </NavMenu>
+                    <UserImg src = {userPhoto} alt="user-display-picture" onClick={handleLogout}/>
+                </>
+            }
+            
         </Nav>
     )
 }
@@ -182,6 +226,14 @@ const Login = styled.a`
         
     }
 
+
+`
+
+const UserImg  =styled.img`
+
+    height: 70%;
+    border-radius: 50%;
+    cursor: pointer;
 
 `
 
