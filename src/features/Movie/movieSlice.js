67ecf1b090121bec9eeb3539718da3cbb7509b8c
rohanrony250 <rohanrony@hotmail.com>
2021-05-27 00:45:55 +0300

    movie-json_data

--- src/features/Movie/movieSlice.js
+++ src/features/Movie/movieSlice.js
@@ -39,3 +39,5 @@ export const selectRecommend = state => state.movie.recommend
 export const selectNewDisney = state => state.movie.newDisney
 export const selectOriginal = state => state.movie.original
 export const selectTrending = state => state.movie.trending
+
+export default movieSlice.reducer 
